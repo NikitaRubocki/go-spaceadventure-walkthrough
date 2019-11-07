1049c8feebd6b9540e477f1024a7b947a8fccb5b
Nikita Rubocki <nikita.rubocki@gmail.com>
2019-11-07 14:27:21 -0800

    Add a Start() function that is called in main and runs the entire spaceadventure program when invoked.

--- cmd/spaceadventure/main.go
+++ cmd/spaceadventure/main.go
@@ -1,13 +1,8 @@
 package main
 
-import "fmt"
-
 import "github.com/NikitaRubocki/go-spaceadventure-walkthrough/internal/spaceadventure"
 
 func main() {
-	spaceadventure.PrintWelcome()
-	spaceadventure.PrintGreeting(spaceadventure.GetResponseToPrompt("What is your name?"))
-	fmt.Println("Let's go on an adventure!")
-	spaceadventure.Travel()
+	spaceadventure.Start()
 }
 
--- internal/spaceadventure/spaceadventure.go
+++ internal/spaceadventure/spaceadventure.go
@@ -2,47 +2,49 @@ package spaceadventure
 
 import "fmt"
 
-// PrintWelcome prints a welcome
-func PrintWelcome() {
+// Start runs the space adventure
+func Start() {
+	printWelcome()
+	printGreeting(getResponseToPrompt("What is your name?"))
+	fmt.Println("Let's go on an adventure!")
+	travel()
+}
+
+func printWelcome() {
 	fmt.Println("Welcome to the Solar System!")
 	fmt.Println("There are 8 planets to explore.")
 }
 
-// GetResponseToPrompt takes a prompt and returns a response
-func GetResponseToPrompt(prompt string) (response string) {
+func getResponseToPrompt(prompt string) (response string) {
 	fmt.Println(prompt)
     fmt.Scan(&response)
     return 
 }
 
-// PrintGreeting takes a name and prints a greeting
-func PrintGreeting(name string) {
+func printGreeting(name string) {
 	fmt.Printf("Nice to meet you, %s. My name is Eliza, I'm an old friend of Siri.\n", name)
 }
 
-// Travel asks whether a random planet should be chosen, or if the user should pick
-func Travel() {
+func travel() {
 	var choice string
 	for choice != "Y" && choice != "N" {
-		choice = GetResponseToPrompt("Shall I randomly choose a planet for you to visit? (Y or N)")
+		choice = getResponseToPrompt("Shall I randomly choose a planet for you to visit? (Y or N)")
 		if choice == "Y" {
-			TravelToRandomPlanet()
+			travelToRandomPlanet()
 		} else if choice == "N" {
-			TravelToPlanet(GetResponseToPrompt("Name the planet you would like to visit."))
+			travelToPlanet(getResponseToPrompt("Name the planet you would like to visit."))
 		} else {
 			fmt.Println("Sorry, I didn't get that.")
 		}
 	}
 }
 
-// TravelToRandomPlanet prints a random planet
-func TravelToRandomPlanet() {
+func travelToRandomPlanet() {
 	fmt.Println("Traveling to Jupiter...")
 	fmt.Println("Arrived at Jupiter. The large red spot appears ominous.")
 }
 
-// TravelToPlanet prints the selected planet
-func TravelToPlanet(planetName string) {
+func travelToPlanet(planetName string) {
 	fmt.Printf("Traveling to %s...\n", planetName)
 	fmt.Println("Arrived at Neptune. A very cold planet, furthest from the sun.")
 }
